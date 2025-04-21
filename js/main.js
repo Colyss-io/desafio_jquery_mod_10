c8e685ccb7c240a79a75af6c23dcf3db2959ede1
Gabriel Amaral <gabrielfcamaral@gmail.com>
2025-04-21 19:51:49 -0300

    ajuste na validaçãoo commit

--- js/main.js
+++ js/main.js
@@ -10,8 +10,7 @@ $(document).ready(function () {
     })
 })
 
-$('#telefone').mask('(00) 00000-0000', {
-})
+$('#telefone').mask('(00) 00000-0000')
 
 $('#cpf').mask('000.000.000-00', {
     placeholder: '123.456.789-00'
